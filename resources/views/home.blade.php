755ef14db12cfd2ff667bcfb082bcff28075fa33
nicolozibra1 <nico.zibra@gmail.com>
2023-05-21 16:31:15 +0200

    added-homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,13 +10,80 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
         <style>
+            *{
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+
+            }
             body {
                 font-family: 'Nunito', sans-serif;
+                background: rgb(18,18,18);
+                background: linear-gradient(17deg, rgb(0, 0, 0) 20%, rgb(255, 0, 0) 100%);
+                height: 100vh;
+                color: white;
+            }
+            header{
+                background-color: rgba(255, 255, 255, 0.678);
+                height: 70px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                width: 50%;
+                margin: auto;
+                border-bottom-left-radius: 10px;
+                border-bottom-right-radius: 10px;
+            }
+            ul {
+                list-style: none;
+                display: flex;
+                gap: 20px;
+            }
+            a{
+                text-decoration: none;
+                text-transform: uppercase;
+                color: black;
+                font-weight: bold;
+            }
+            main{
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                height: calc(100% - 120px);
+                padding: 0 450px;
+                position: relative;
+            }
+            h1{
+                color: white;
+                font-size: 5rem;
+                text-transform: uppercase;
+            }
+            p{
+                padding-top: 10px;
+                text-decoration: underline;
+            }
+            img{
+                position: absolute;
+                top: 200px;
+                right: 500px;
+                width: 20%;
             }
         </style>
     </head>
 
     <body class="antialiased">
-        <h1>{{$page_title}}</h1>
+        <header>
+            <ul>
+                @foreach ($nav_links as $link)
+                    <li><a href="{{route($link)}}">{{$link}}</a></li>
+                @endforeach
+            </ul>
+        </header>
+        <main>
+            <h1>{{$page_title}}</h1>
+            <h3>Laravel - Primi Passi</h3>
+            <p>Discover the ROUTE functionality by clicking on the links in the navigation bar to access other pages!</p>
+            <img src="../img/logo-laravel-grey.png" alt="">
+        </main>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,66 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $data = [
-        'page_title' => 'Hello World'
+        'page_title' => 'Hello World!',
+        'nav_links' => [
+            'home',
+            'course',
+            'students',
+            'about'
+        ]
     ];
     return view('home', $data);
-});
+})->name('home');
+
+Route::get('/course', function () {
+    $page_title = 'My Course';
+    $course_name = 'Boolean';
+    $course_type = 'Web Developer Full Stack';
+    return view('course', compact('page_title', 'course_name', 'course_type'));
+})->name('course');
+
+Route::get('/students', function () {
+    $data = [
+        $class_number = 'Classe 92',
+        'students' => [
+            'alessandro garganese',
+            'alessandro pagliuca',
+            'alessio logozzo',
+            'anas hamimi',
+            'andrea collura',
+            'aniello rossi',
+            'arianna drovandi',
+            'chiara sucato',
+            'christian paliotta moretti',
+            'christian totaro',
+            'daniele ceravolo',
+            'davide caputo',
+            'davide sberna',
+            'emanuele rosella musico',
+            'filippo giampapa',
+            'filippo verrone',
+            'francesco froechlich',
+            'gaetano spampinato',
+            'gerson membrillo',
+            'gianmarco incocciati',
+            'giorgio lumia',
+            'giovanni jacopo speranza',
+            'giuseppe indomenico',
+            'giuseppe lucà',
+            'jorge castillo tenasoa',
+            'marco bione',
+            'marika di blasio',
+            'matteo aguiari',
+            'michele libralato',
+            'nicola di quinzio',
+            'nicolò zibra',
+            'raffaele capaldo',
+            'taddeo neri',
+            'vito de palma',
+        ]
+    ];
+})->name('students');
+
+Route::get('/about', function () {
+    $description = "I'm a Web Developer based in Milan (Italy). I attend the Boolean course and I'm a student of class 92";
+})->name('about');
