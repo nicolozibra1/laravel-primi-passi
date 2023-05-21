c230f5a3b02bf22237b51f8abf7acc2e6802aba8
nicolozibra1 <nico.zibra@gmail.com>
2023-05-21 15:01:54 +0200

    printed-hello-world

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = [
+        'page_title' => 'Hello World'
+    ];
+    return view('home', $data);
 });
